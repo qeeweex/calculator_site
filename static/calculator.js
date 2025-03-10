81be5ee06411cb234f7ca3126c8d2669786995d4
unknown <garmashkir09@mail.ru>
2025-03-10 12:33:18 +0300

    Добавил новые кнопки а также изменил дизайн для калькулятора

--- static/calculator.js
+++ static/calculator.js
@@ -39,6 +39,30 @@ function minus() {
     operation = "-";
 }
 
+function square_root() {
+    first_number = get_number()
+    clear_input();
+    operation = "√";
+}
+
+function sin() {
+    first_number = get_number()
+    clear_input();
+    operation = "sin";
+}
+
+function cos() {
+    first_number = get_number()
+    clear_input();
+    operation = "cos";
+}
+
+function tan() {
+    first_number = get_number()
+    clear_input();
+    operation = "tan";
+}
+
 function equal() { 
     let second_number = get_number();
 
@@ -56,6 +80,18 @@ function equal() {
     else if (operation == "/") {
         result = first_number / second_number;
 
+    }
+    else if (operation == "√") {
+        result = Math.sqrt(first_number) 
+    }
+    else if (operation == "sin") {
+        result = Math.sin(first_number)
+    }
+    else if (operation == "cos") { 
+        result = Math.cos(first_number)
+    }
+    else if (operation == "tan") {
+        result = Math.tan(first_number)
     }
     else if (operation == undefined) {
         alert("Вы не выбрали, какую операцию выполнять!")
